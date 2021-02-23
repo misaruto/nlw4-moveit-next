85f216957e0806c3d490284f3b8e6a6a82e4d0dd
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2021-02-23 11:50:16 -0300

    Firs commit, created news components for perfil, completed challenges, experiece bar and countdown

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from "../styles/components/Profile.module.css";
+const Profile = () => {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/misaruto.png" />
+      <div>
+        <strong>Misael Guilhardes de Freitas</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
