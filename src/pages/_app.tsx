4a768a3840b61190bc0b61cf51a77743c2c2d177
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2021-02-25 20:38:29 -0300

    Changing use of countdown provider from app.tsx to inde.tsx - home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import { ChallengesProvider } from "../contexts/ChallengesContext";
-import { CountdownProvider } from "../contexts/CountDownContext";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
-      <CountdownProvider>
-        <Component {...pageProps} />
-      </CountdownProvider>
+      <Component {...pageProps} />
     </ChallengesProvider>
   );
 }
