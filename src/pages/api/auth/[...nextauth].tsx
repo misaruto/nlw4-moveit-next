317c78026103a73f2e3446d659268cb35e21100c
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2021-08-09 00:56:19 -0300

    Voltando com os callbacks

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -2,12 +2,25 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      return true;
+    },
+    async redirect(url, baseUrl) {
+      return baseUrl;
+    },
+    async session(session, user) {
+      return session;
+    },
+    async jwt(token, user, account, profile, isNewUser) {
+      return token;
+    },
+  },
 });
