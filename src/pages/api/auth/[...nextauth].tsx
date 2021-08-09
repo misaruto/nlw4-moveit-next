0abed284695bfd3d193e53064a51281953752b79
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2021-08-09 00:49:01 -0300

    Removendo callbacks

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -10,24 +10,4 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  callbacks: {
-    /**
-     * @param  {string} url      URL provided as callback URL by the client
-     * @param  {string} baseUrl  Default base URL of site (can be used as fallback)
-     * @return {string}          URL the client will be redirect to
-     */
-    async redirect(url, baseUrl) {
-      return url.startsWith(baseUrl) ? url : baseUrl;
-    },
-    async signIn(user, account, profile) {
-      return true;
-    },
-    async session(session, user) {
-      localStorage.setItem('use', JSON.stringify(user));
-      return session;
-    },
-    async jwt(token, user, account, profile, isNewUser) {
-      return token;
-    },
-  },
 });
